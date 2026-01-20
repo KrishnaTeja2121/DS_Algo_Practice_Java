2222654d335a1b0161b6da9716c8cca7ad65068e
KrishnaTeja2121 <37261298+KrishnaTeja2121@users.noreply.github.com>
2026-01-20 10:23:43 +0530

    sparsearray

--- src/Arrays/SparseArrays.java
+++ src/Arrays/SparseArrays.java
@@ -1,9 +1,29 @@
 package Arrays;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class SparseArrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		scan.nextLine(); // gets rid of pesky newline
+
+		/* Create and fill HashMap */
+		HashMap<String, Integer> map = new HashMap();
+		for (int i = 0; i < N; i++) {
+			String str = scan.nextLine();
+			map.merge(str, 1, Integer::sum); // equivalent to map.getOrDefault(str,0)+1)
+
+		}
+
+		int Q = scan.nextInt();
+		scan.nextLine(); // gets rid of pesky newline
+		for (int i = 0; i < Q; i++) {
+			String str = scan.nextLine();
+			System.out.println(map.getOrDefault(str, 0));
+		}
 
 	}
 
